8c35abce40156d72c2befdeeed114244ddf768f5
dev1 <dev1@ewanintl.sa>
2023-05-17 16:53:36 +0300

    add finance sector

--- src/app/receipts/payment-receipts/list-payment-receipts/list-payment-receipts.component.ts
+++ src/app/receipts/payment-receipts/list-payment-receipts/list-payment-receipts.component.ts
@@ -25,117 +25,125 @@ export class ListPaymentReceiptsComponent implements OnInit {
   @ViewChild(ReportModelViewerComponent)
   reportchild: ReportModelViewerComponent;
 
-  pageListConfig:PageListConfig;
+  pageListConfig: PageListConfig;
 
   constructor(
     public _dynamicSearchService: DynamicSearchService,
+    private _router: Router,
+    private auth: AuthService
   ) {}
 
   ngOnInit() {
     this.createPageListConfig();
   }
 
-  createPageListConfig(){
+  createPageListConfig() {
     this.pageListConfig = {
-      pageAuthorization:'receipts-payment-receipts-list',
-      pageTitle:'Receipts.Titles.PaymentReceiptsListPage',
-      createAuthorization:'receipts-payment-receipts-create',
-      createButtonTitle:'Receipts.Buttons.PaymentReceiptCreate',
-      createLink:'/finance/receipts/create-payment-receipts',
-      getDataAPIURL:`${environment.financeSectorAPIURL}/v1/PaymentReceipt/GetPagedList`,
-      actions:[
+      pageAuthorization: 'receipts-payment-receipts-list',
+      pageTitle: 'Receipts.Titles.PaymentReceiptsListPage',
+      createAuthorization: 'receipts-payment-receipts-create',
+      createButtonTitle: 'Receipts.Buttons.PaymentReceiptCreate',
+      createLink: '/finance/receipts/create-payment-receipts',
+      getDataAPIURL: `${environment.financeSectorAPIURL}/v1/PaymentReceipt/GetPagedList`,
+      actions: [
         {
-          authorization:'',
-          title:'',
-          callBack:(dataItem)=>{
-            debugger;
+          authorization: 'receipts-payment-receipts-edit',
+          title: 'App.Buttons.Edit',
+          callBack: (dataItem) => {
+            this._router.navigate([
+              '/receipts/edit-payment-receipts',
+              dataItem.id,
+            ]);
           },
-        }
+        },
+        {
+          authorization: 'receipts-payment-receipts-print',
+          title: 'App.Buttons.Print',
+          callBack: (dataItem) => {
+            this.showReport(dataItem.id);
+          },
+        },
       ],
-      cols:[
-          {
-            field: 'code',
-            header: 'Receipts.Fields.ReciptId'
-          },
-          {
-            field: 'documentDate',
-            header: 'Receipts.Fields.ReciptDate',
-            searchType:SearchType.Date,
-            pipe: ColumnPipeType.Date,
-            pipeFormat: ColumnPipeFormat.DatePipeFormat,
-          },
-          {
-            field: 'customerCode',
-            header: 'Receipts.Fields.customerCode',
-            customSearchField:"Customer.Code",
-          },
-          {
-            field: 'customerName',
-            header: 'Receipts.Fields.CustomerName',
-            customSearchField:"Customer.Name",
-            isLocalized:true,
-          },
-          {
-            field: 'entityCode',
-            header: 'Receipts.Fields.ContractId',
-          },
-          {
-            field: 'bankName',
-            header: 'Receipts.Fields.BankAccountName',
-            customSearchField:"BankAccount.Name",
-            isLocalized:true,
-          },
-          {
-            field: 'cashBoxName',
-            header: 'Receipts.Fields.CashBoxName',
-            customSearchField:"CashBox.Name",
-            isLocalized:true,
-          },
-          {
-            field: 'creditCardTypeName',
-            header: 'Receipts.Fields.CreditCardTypeName',
-            customSearchField:"CreditCardType.Name",
-            isLocalized:true,
-          },
-          {
-            field: 'branchName',
-            header: 'App.Fields.Branch',
-            customSearchField:"Branch.Name",
-            isLocalized:true,
-          },
-          {
-            field: 'netValueAfterTax',
-            header: 'Receipts.Fields.ReciptValue',
-          },
-          {
-            field: 'isDownPayment',
-            header: 'Receipts.Fields.ReciptType',
-          },
-          {
-            field: 'tolalPaid',
-            header: 'Receipts.Fields.InvoiceGetPaid',
-          },
-          {
-            field: 'totalRefund',
-            header: 'Receipts.Fields.AllRetreived',
-          },
-        ],
-    }
-  }
-
-  setId(id: string) {
-    //this._router.navigate(['/receipts/edit-payment-receipts', id]);
+      cols: [
+        {
+          field: 'code',
+          header: 'Receipts.Fields.ReciptId',
+        },
+        {
+          field: 'documentDate',
+          header: 'Receipts.Fields.ReciptDate',
+          searchType: SearchType.Date,
+          pipe: ColumnPipeType.Date,
+          pipeFormat: ColumnPipeFormat.DatePipeFormat,
+        },
+        {
+          field: 'customerCode',
+          header: 'Receipts.Fields.customerCode',
+          customSearchField: 'Customer.Code',
+        },
+        {
+          field: 'customerName',
+          header: 'Receipts.Fields.CustomerName',
+          customSearchField: 'Customer.Name',
+          isLocalized: true,
+        },
+        {
+          field: 'entityCode',
+          header: 'Receipts.Fields.ContractId',
+        },
+        {
+          field: 'bankName',
+          header: 'Receipts.Fields.BankAccountName',
+          customSearchField: 'BankAccount.Name',
+          isLocalized: true,
+        },
+        {
+          field: 'cashBoxName',
+          header: 'Receipts.Fields.CashBoxName',
+          customSearchField: 'CashBox.Name',
+          isLocalized: true,
+        },
+        {
+          field: 'creditCardTypeName',
+          header: 'Receipts.Fields.CreditCardTypeName',
+          customSearchField: 'CreditCardType.Name',
+          isLocalized: true,
+        },
+        {
+          field: 'branchName',
+          header: 'App.Fields.Branch',
+          customSearchField: 'Branch.Name',
+          isLocalized: true,
+        },
+        {
+          field: 'netValueAfterTax',
+          header: 'Receipts.Fields.ReciptValue',
+        },
+        {
+          field: 'isDownPayment',
+          header: 'Receipts.Fields.ReciptType',
+        },
+        {
+          field: 'tolalPaid',
+          header: 'Receipts.Fields.InvoiceGetPaid',
+        },
+        {
+          field: 'totalRefund',
+          header: 'Receipts.Fields.AllRetreived',
+        },
+      ],
+    };
   }
 
   showReport(paymentId) {
-    // this.reportchild.reportName = 'Receipts.Titles.PaymentReceiptsListPage';
-    // this.auth.getAuthUser().subscribe((result) => {
-    //   const user = result.data;
-    //   this.reportchild.showReprot(
-    //     8,
-    //     `&Ds1_Filter1=And,CreditReceivableId,=,${paymentId}&UserName=${user.ArabicFullName}`,
-    //     false
-    //   );
-    // });
+    this.reportchild.reportName = 'Receipts.Titles.PaymentReceiptsListPage';
+    this.auth.getAuthUser().subscribe((result) => {
+      const user = result.data;
+      this.reportchild.showReprot(
+        8,
+        `&Ds1_Filter1=And,CreditReceivableId,=,${paymentId}&UserName=${user.ArabicFullName}`,
+        false
+      );
+    });
   }
 }
