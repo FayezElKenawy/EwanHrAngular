cb41cd3592fb09c45755f9b5bd9c2d34193c3de0
Amr <dev3@ewanintl.sa>
2023-05-17 10:08:28 +0300

    debit-payment

---
+++ src/app/shared/services/bank-account.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BankAccountService {
+  serviceUrl: string = environment.financeURL + '/v1/BankAccount';
+
+  constructor(
+    private _http: HttpClient
+  ) {}
+
+  getAll(searchTerm:string):Observable<any>{
+    return this._http.get<any>(`${this.serviceUrl}/GetSelectList`, {
+      params: { searchTerm: searchTerm },
+    });
+  }
+}
