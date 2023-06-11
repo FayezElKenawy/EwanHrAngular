453b2035259d669e9293c8d6474a91cd1fc247c3
dev1 <dev1@ewanintl.sa>
2023-06-11 11:44:52 +0300

    edit url port

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -3,15 +3,15 @@ export const environment = {
   name: 'App.Titles.DevelopingVersion',
   hostingFolder: "/ERP.Finance",
 
-  coreApiUrl: 'http://ewan1.uksouth.cloudapp.azure.com:64/BroaERP.Core.API',
-  reportingApiUrl: 'http://ewan1.uksouth.cloudapp.azure.com:64/BroaERP.Reporting.API',
-  financeSectorAPIURL:"http://ewan1.uksouth.cloudapp.azure.com:64/Ewan.Finance.API/api",
+  coreApiUrl: 'http://ewan1.uksouth.cloudapp.azure.com:62/BroaERP.Core.API',
+  reportingApiUrl: 'http://ewan1.uksouth.cloudapp.azure.com:62/BroaERP.Reporting.API',
+  financeSectorAPIURL:"http://ewan1.uksouth.cloudapp.azure.com:62/Ewan.Finance.API/api",
   
-  coreUrl: "http://ewan1.uksouth.cloudapp.azure.com:64/ERP.Core",
-  businessSectorURL: "http://ewan1.uksouth.cloudapp.azure.com:64/ERP.BusinessSector",
-  houseLabourSectorUrl: "http://ewan1.uksouth.cloudapp.azure.com:64/ERP.HouseLabourSector",
-  individualSectorURL: "http://ewan1.uksouth.cloudapp.azure.com:64/ERP.IndividualSector",
-  financeURL:"http://ewan1.uksouth.cloudapp.azure.com:64/ERP.Finance",
+  coreUrl: "http://ewan1.uksouth.cloudapp.azure.com:62/ERP.Core",
+  businessSectorURL: "http://ewan1.uksouth.cloudapp.azure.com:62/ERP.BusinessSector",
+  houseLabourSectorUrl: "http://ewan1.uksouth.cloudapp.azure.com:62/ERP.HouseLabourSector",
+  individualSectorURL: "http://ewan1.uksouth.cloudapp.azure.com:62/ERP.IndividualSector",
+  financeURL:"http://ewan1.uksouth.cloudapp.azure.com:62/ERP.Finance",
 
   storagePath: 'http://localhost/BroaERP.Core.API/Uploading/File/Public',
   storagePhysicalPath: "C:\\inetpub\\BroaERP\\Developing\\UploadFolder\\",
